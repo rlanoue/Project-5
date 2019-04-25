6f2ee01038b24bd2cdc01d106c949885b9f90656
Jordan Lanoue <rlanoue@ou.edu>
2019-04-24 19:40:49 -0500

    second

---
+++ Todo.java
@@ -0,0 +1,36 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-04-23
+OU ID_______________: 113475042
+OU EMAIL____________: rlanoue@ou.edu
+
+Tasks:
+1. Create the Regions
+2. Draw Bounding Boxes
+3. Draw Translucent Rectangle
+4. MouseClicked Override 
+5. setSelectedRegion Contents
+6. Evaluate method 
+7. Add X to panels 
+8. ActionListeners
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 5
+4. 20
+5. 20
+6. 20
+7. 25
+8. 20
+
+Actual Time (minutes):
+1. 20
+2. 15
+3. 5
+4. 20
+5. 40
+6. 10
+7. 20
+8. 35
+*/}
